57f9b3f1d31f43c93766193c986ec4c93bc9e6be
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-13 18:09:25 +0300

    Update ContactForm.styled.jsx

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import { Form, Field } from 'formik';
 
 export const StyledForm = styled(Form)`
+  position: relative;
   display: inline-flex;
   flex-direction: column;
   padding: 20px;
@@ -16,5 +17,21 @@ export const FormLabel = styled.label`
 `;
 
 export const Input = styled(Field)`
-  margin-bottom: 5px;
+  margin-bottom: 25px;
+`;
+
+export const ErrorTextName = styled.p`
+  position: absolute;
+  top: 70px;
+  width: 200px;
+  margin: 0;
+  color: red;
+`;
+
+export const ErrorTextNumber = styled.p`
+  position: absolute;
+  top: 147px;
+  width: 200px;
+  margin: 0;
+  color: red;
 `;
